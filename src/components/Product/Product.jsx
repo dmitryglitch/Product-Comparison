2b9f5a0c5429abb00b92f36dfc393825ee262b6b
dmitryglitch <brgdtv121>
2020-01-21 22:07:34 +0300

    New 0.1

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Product = ({data}) => (
+    <div>
+        1
+        {data.result}
+    </div>
+);
+
+export default Product;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import Product from '../components/Product/Product'
 
 import {connect} from 'react-redux'
 import {getCoupleProducts} from '../actions/product'
 
 class App extends Component {
+    componentDidMount() {
+        this.props.onGetCoupleProducts();
+    }
+
     render() {
         return (
             <div className="product-container">
                 <button onClick={this.props.onGetCoupleProducts}/>
+                <Product data={this.props.products}/>
                 <div className="product-content">
                 </div>
             </div>
