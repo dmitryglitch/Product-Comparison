b26211464991ce7720247739a45e809f49e9e5d8
dmitryglitch <brgdtv121>
2020-01-23 03:54:15 +0300

    Changed method generation a product components

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,17 +7,29 @@ import {connect} from 'react-redux'
 import {getCoupleProducts} from '../actions/product'
 
 class App extends Component {
+
     componentDidMount() {
         this.props.onGetCoupleProducts();
+    }
+
+    renderProducts() {
+        const {massProducts} = this.props.products;
+
+        if (massProducts !== null) {
+            return Object.keys(massProducts).map(products => {
+                return (
+                    <div className="product-container-item" key={massProducts[products].id + 'pci'}>
+                        <Product data={massProducts[products]} key={massProducts[products].id}/>
+                    </div>
+                )
+            })
+        }
     }
 
     render() {
         return (
             <div className="product-container">
-                <button onClick={this.props.onGetCoupleProducts}/>
-                <Product data={this.props.products}/>
-                <div className="product-content">
-                </div>
+                {this.renderProducts()}
             </div>
         );
     }
