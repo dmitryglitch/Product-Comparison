d13adf6a2d90f40d37318367d814c8ed3a6a8706
dmitryglitch <brgdtv121>
2020-02-26 04:49:49 +0500

    Add new func check user status

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,3 +1,33 @@
+import { getCoupleProducts } from "./product";
+
+export const checkUserStatus = () => dispatch => {
+  dispatch({ type: "CHECK_USER_STATUS_STARTED" });
+
+  fetch("http://match.protarget.pro/backend/admin/chekUserStatus.php", {
+    method: "POST",
+    credentials: "same-origin",
+    headers: { Accept: "application/json" }
+  })
+    .then(res => res.json())
+    .then(json => {
+      console.log("checkUserStatus", json);
+      if (json.result === "ok") {
+        dispatch({ type: "CHECK_USER_STATUS_SUCCESS", payload: json });
+
+        dispatch(getCoupleProducts(json.privilege));
+
+        return true;
+      } else {
+        // console.log(json.errtext);
+        alert(json.errtext);
+        return false;
+      }
+    })
+    .catch(error =>
+      dispatch({ type: "CHECK_USER_STATUS_ERROR", payload: error })
+    );
+};
+
 export const sendUserLogin = (userName, password) => dispatch => {
   dispatch({ type: "SEND_LOGIN_USER_STARTED" });
 
@@ -5,7 +35,7 @@ export const sendUserLogin = (userName, password) => dispatch => {
   data.append("userName", userName);
   data.append("password", password);
 
-  fetch("http://match-dev.protarget.pro/backend/isLogined.php", {
+  fetch("http://match.protarget.pro/backend/isLogined.php", {
     method: "POST",
     credentials: "same-origin",
     headers: { Accept: "application/json" },
@@ -13,7 +43,7 @@ export const sendUserLogin = (userName, password) => dispatch => {
   })
     .then(res => res.json())
     .then(json => {
-      console.log("sendUserLogin",json);
+      console.log("sendUserLogin", json);
       if (json.result === "ok") {
         dispatch({ type: "SEND_LOGIN_USER_SUCCESS", payload: json });
         window.location.reload();
@@ -30,10 +60,10 @@ export const sendUserLogin = (userName, password) => dispatch => {
 export const logOut = (userName, password) => dispatch => {
   dispatch({ type: "LOGOUT_USER_STARTED" });
 
-  fetch("http://match-dev.protarget.pro/backend/logOut.php", {
+  fetch("http://match.protarget.pro/backend/logOut.php", {
     method: "POST",
     credentials: "same-origin",
-    headers: { Accept: "application/json" },
+    headers: { Accept: "application/json" }
   })
     .then(res => res.json())
     .then(json => {
