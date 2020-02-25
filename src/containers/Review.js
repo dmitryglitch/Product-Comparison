3074bf476e8bbb2199cf3a586e6aeebcc0e24f78
dmitryglitch <brgdtv121>
2020-02-26 04:58:33 +0500

    Add new func and handler func - Get New Portions

--- src/containers/Review.js
+++ src/containers/Review.js
@@ -2,6 +2,7 @@ import { connect } from "react-redux";
 
 import {
   getListUser,
+  getNewPortions,
   updateSelectedUser,
   updateDateStarted,
   updateDateEnding,
@@ -10,12 +11,22 @@ import {
 } from "/Projects/proTarget/match/src/actions/review";
 
 import React, { Component } from "react";
-import Inputs from "./Inputs";
-import ListUser from "./ListUser";
+import Inputs from "../components/Review/Inputs";
+import ListUser from "../components/Review/ListUser";
 
 class Review extends Component {
   componentDidMount() {
     this.props.onGetListUsers();
+  }
+
+  handleClick = () => {
+    const selectedUser = this.props.review.selectedUser
+    const dateStarted = this.props.review.dateStarted
+    const dateEnding = this.props.review.dateEnding
+    const servingSize = this.props.review.servingSize
+    const percentVerified = this.props.review.percentVerified
+
+    this.props.onGetNewPortions(selectedUser, dateStarted, dateEnding, servingSize, percentVerified)
   }
 
   render() {
@@ -27,13 +38,14 @@ class Review extends Component {
         </div>
         <div className="review-content">
           <div className="review-content-left">
-            <Inputs key="inputs"
+            <Inputs
+              key="inputs"
               updateDateStarted={this.props.onUpdateDateStarted}
               updateDateEnding={this.props.onUpdateDateEnding}
               updateServingSize={this.props.onUpdateServingSize}
               updatePercentVerified={this.props.onUpdatePercentVerified}
             />
-            <button>Serve the database</button>
+            <button onClick={this.handleClick}>Serve the database</button>
           </div>
           <div className="review-content-right">
             <ListUser
@@ -52,6 +64,25 @@ export default connect(
     review: state.review
   }),
   dispatch => ({
+    // создание порции товаров
+    onGetNewPortions: (
+      selectedUser,
+      dateStarted,
+      dateEnding,
+      servingSize,
+      percentVerified
+    ) => {
+      dispatch(
+        getNewPortions(
+          selectedUser,
+          dateStarted,
+          dateEnding,
+          servingSize,
+          percentVerified
+        )
+      );
+    },
+    // получение списка всех пользователей
     onGetListUsers: () => {
       dispatch(getListUser());
     },
