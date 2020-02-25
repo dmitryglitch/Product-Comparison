cde8d23f1a3f0d54d0669942f607c24ce6f53d15
dmitryglitch <brgdtv121>
2020-02-26 04:49:24 +0500

    Add new func get new portions

--- src/actions/review.js
+++ src/actions/review.js
@@ -1,7 +1,7 @@
 export const getListUser = () => dispatch => {
   dispatch({ type: "GET_LIST_USER_STARTED" });
 
-  fetch("http://match-dev.protarget.pro/backend/admin/getUserList.php", {
+  fetch("http://match.protarget.pro/backend/admin/getUserList.php", {
     method: "POST",
     credentials: "same-origin",
     headers: { Accept: "application/json" }
@@ -17,8 +17,66 @@ export const getListUser = () => dispatch => {
     });
 };
 
+export const getNewPortions = (
+  selectedUser,
+  dateStarted,
+  dateEnding,
+  servingSize,
+  percentVerified
+) => dispatch => {
+
+  let data = new FormData();
+  data.append("selectedUser", selectedUser);
+  data.append("dateStarted", dateStarted);
+  data.append("dateEnding", dateEnding);
+  data.append("servingSize", servingSize);
+  data.append("percentVerified", percentVerified);
+
+  dispatch({ type: "GET_NEW_PORTION_STARTED" });
+  dispatch({ type: "GET_COUPLE_PRODUCTS_STATED" });
+
+  fetch("http://match.protarget.pro/backend/admin/getNewPortions.php", {
+    method: "POST",
+    body: data,
+    credentials: "same-origin",
+    headers: { Accept: "application/json" }
+  })
+    .then(res => res.json())
+    .then(json => {
+      if (json.result === "Ok") {
+        let data = {
+          id: json.id,
+          date: json.date,
+          id_answer: json.id_answer,
+          answerUser: json.answerUser,
+          userName: json.userName,
+          result: json.result,
+          statistics: json.statistics,
+          massProducts: {
+            oneProduct: json.oneProduct,
+            twoProduct: json.newTwoProduct
+          }
+        };
+        dispatch({ type: "GET_NEW_PORTION_SUCCESS" });
+        dispatch({ type: "GET_COUPLE_PRODUCTS_SUCCESS", payload: data });
+        dispatch({ type: "SEND_LOGIN_USER_SUCCESS", payload: json.userName });
+      } else {
+        dispatch({ type: "GET_COUPLE_PRODUCTS_LOGIN_ERROR" });
+      }
+      console.log("getListUser", json);
+    })
+    .catch(error => {
+      dispatch({ type: "GET_NEW_PORTION_ERROR", payload: error });
+      alert(error);
+    });
+};
+
 export const updateSelectedUser = newValue => {
-  return { type: "UPDATE_CURRENT_SELECTED_USER", payload: newValue };
+  if (newValue === "По всем") {
+    return { type: "UPDATE_CURRENT_SELECTED_USER", payload: 0 };
+  } else {
+    return { type: "UPDATE_CURRENT_SELECTED_USER", payload: newValue };
+  }
 };
 
 export const updateDateStarted = newValue => {
@@ -36,4 +94,3 @@ export const updateServingSize = newValue => {
 export const updatePercentVerified = newValue => {
   return { type: "UPDATE_PERCENT_VERIFIED", payload: newValue };
 };
-
