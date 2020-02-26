288ef095dc3c7d221599b9ebe560a82857b4b036
dmitryglitch <brgdtv121>
2020-02-26 05:05:25 +0500

    Add new case for action type(check user status)

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,5 +1,5 @@
 const initialState = {
-  userName: "",
+  userName: null,
   privilege: null,
   isLogin: false,
   isFetching: true,
@@ -8,11 +8,21 @@ const initialState = {
 
 const user = (state = initialState, action) => {
   switch (action.type) {
+    case "CHECK_USER_STATUS_SUCCESS":
+      return {
+        ...state,
+        userName: action.payload.userName,
+        privilege: action.payload.privilege,
+        isFetching: false,
+        isError: null,
+        isLogin: true
+      };
+
     case "SEND_LOGIN_USER_SUCCESS":
       return {
         ...state,
-        userName: action.payload,
-        // privilege: action.payload.privilege,
+        userName: action.payload.userName,
+        privilege: action.payload.privilege,
         isFetching: false,
         isError: null,
         isLogin: true
@@ -27,18 +37,22 @@ const user = (state = initialState, action) => {
         isLogin: false
       };
 
+    case "CHECK_USER_STATUS_STARTED":
     case "SEND_LOGIN_USER_STARTED":
     case "LOGOUT_USER_STARTED":
       return {
         ...state,
+        isLogin: false,
         isFetching: true,
         isError: null
       };
 
+    case "CHECK_USER_STATUS_ERROR":
     case "SEND_LOGIN_USER_ERROR":
     case "LOGOUT_USER_ERROR":
       return {
         ...state,
+        isLogin: false,
         isFetching: false,
         isError: action.payload
       };
